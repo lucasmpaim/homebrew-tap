24423cdd238fed0512421f3ba01aed7d2a0d0cc9
Lucas Mrowskovsky Paim <lucasmpaim1@gmail.com>
2020-04-01 22:45:37 -0300

    Update danger-js.rb

--- danger-js.rb
+++ danger-js.rb
@@ -1,6 +1,6 @@
 class DangerJs < Formula
   homepage "https://github.com/lucasmpaim/danger-js"
-  url "https://github.com/danger/danger-js/releases/download/10.0.1/danger-macos.zip"
+  url "https://github.com/lucasmpaim/danger-js/releases/download/10.0.1/danger-macos.zip"
   sha256 "bd3a641c439225ad0136fecdce5bdb133449f099bf6cfb4b811ebee140583a47"
 
   def install
